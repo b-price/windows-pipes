bf1dd3f0b41701e957331a268c312fcb09b73c7a
Ben Price <bpricedev@gmail.com>
2024-03-08 10:40:13 -0500

    Added comments

--- pipeClient.cpp
+++ pipeClient.cpp
@@ -1,16 +1,33 @@
+/**
+ * @file pipeClient.cpp
+ * @author Ben Price
+ * @brief A client program to receive a string message from a pipe
+ * @version 1.0
+ * @date 2024-03-08
+ * 
+ * 
+ * 
+ */
+
 #include <stdio.h>
 #include <windows.h>
 #define BUFFER_SIZE 25
 
 int main(VOID) {
+    /** 
+     * Declaration of needed parameters*/
     HANDLE readHandle;
     CHAR charBuffer[BUFFER_SIZE];
     DWORD read;
 
-    /* get the read handle of the pipe */
+    /**
+     * Get the read handle of the pipe */
     readHandle = GetStdHandle(STD_INPUT_HANDLE);
     
-    /* the child reads from the pipe */
+    /**
+     * @brief Read contents of pipe from parent process
+     * 
+     */
     if (ReadFile(readHandle, charBuffer, BUFFER_SIZE, &read, NULL))
         printf("Read from parent:  %s", charBuffer);
     else
--- pipeServer.cpp
+++ pipeServer.cpp
@@ -1,9 +1,22 @@
+/**
+ * @file pipeServer.cpp
+ * @author Ben Price
+ * @brief Program (server) to send anonymous pipe handle to client
+ * @version 1.0
+ * @date 2024-03-08
+ * 
+ * 
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-
 #define BUFFER_SIZE 25
+
 int main(VOID) {
+    /** 
+     * Declaration of needed parameters*/
     HANDLE readHandle, writeHandle;
     STARTUPINFO strtInfo;
     PROCESS_INFORMATION procInfo;
@@ -11,46 +24,57 @@ int main(VOID) {
     DWORD written;
     LPCSTR executablePath = "pipeClient.exe";
 
-    /* set up security attributes allowing pipes to be inherited */
+    /**
+     *  Set up security attributes so child can inherit */
     SECURITY_ATTRIBUTES sa = {sizeof(SECURITY_ATTRIBUTES),NULL,TRUE};
 
-    /* allocate memory */
+    /** 
+     * Memory allocation */
     ZeroMemory(&procInfo, sizeof(procInfo));
 
-    /* create the pipe */
+    /** 
+     * Create the pipe. Notifies of errors */
     if (!CreatePipe(&readHandle, &writeHandle, &sa, 0)) {
         fprintf(stderr, "Create Pipe Failure");
         return 1;
     }
 
-    /* establish the START INFO structure for the child process */
+    /** 
+     * Set up START_INFO structure for child */
     GetStartupInfo(&strtInfo);
     strtInfo.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    /* redirect standard input to the read end of the pipe */
+    /** 
+     * Change client's standard input to pipe read end */
     strtInfo.hStdInput = readHandle;
     strtInfo.dwFlags = STARTF_USESTDHANDLES;
 
-    /* don’t allow the child to inherit the write end of pipe */
+    /** 
+     * Prevent child from inheriting write end of pipe */
     SetHandleInformation(writeHandle, HANDLE_FLAG_INHERIT, 0);
 
-    /* create the child process */
+    /** 
+     * Create the child process,
+     * allowing inheritance of handles */
     CreateProcess(executablePath, NULL, NULL, NULL,
-        TRUE, 
-        /* inherit handles */
-        0, NULL, NULL, &strtInfo, &procInfo);
+        TRUE, 0, NULL, NULL, &strtInfo, &procInfo);
 
-    /* close the unused end of the pipe */
+    /** 
+     * Close the parent's unused read end of the pipe */
     CloseHandle(readHandle);
 
-    /* the parent writes to the pipe */
+    /**
+     * Write the message to the pipe,
+     * handling errors */
     if (!WriteFile(writeHandle, message, BUFFER_SIZE, &written, NULL))
         fprintf(stderr, "Error writing to pipe.");
 
-    /* close the write end of the pipe */
+    /** 
+     * Close the parent's write end of the pipe */
     CloseHandle(writeHandle);
 
-    /* wait for the child to exit */
+    /** 
+     * Await termination of child process */
     WaitForSingleObject(procInfo.hProcess, INFINITE);
     CloseHandle(procInfo.hProcess);
     CloseHandle(procInfo.hThread);
